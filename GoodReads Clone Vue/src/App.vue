ac6c0d8478aca50013e94c39f5683a864fde347b
Tim Cafarella <tcaf.351@gmail.com>
2025-04-25 14:00:44 +1000

    Header and Footer components setup

--- GoodReads Clone Vue/src/App.vue
+++ GoodReads Clone Vue/src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+  import Header from './components/Header.vue';
+  import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <h1>Vue GoodReads Clone</h1>
+  <Header message="Hello from the Header" />
+  <Footer message="Hello from the Footer" />
 </template>
 
 <style scoped>
