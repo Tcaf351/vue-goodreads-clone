e6d1d6c07c1900b41df9895acfe76d66b5f5bb59
Tim Cafarella <tcaf.351@gmail.com>
2025-04-28 07:27:36 +1000

    added bookmark icon to Footer.vue and also added styling for the footer

--- GoodReads Clone Vue/src/components/Footer.vue
+++ GoodReads Clone Vue/src/components/Footer.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>
-        <h1>{{ message }}</h1>
-    </div>
+    <footer class="flex items-start justify-center bg-[#E4E2D5] h-16 rounded-b-md">
+        <div class="border-solid border-t-2  border-gray-900 py-2 px-4 text-center w-24">
+            <img 
+              src="../../public/bookmark-white.png" 
+              alt="book unicode" 
+              class="mx-auto h-6">
+            <span class="text-xs">{{ message }}</span>
+          </div>
+    </footer>
 </template>
 
 <script setup>
